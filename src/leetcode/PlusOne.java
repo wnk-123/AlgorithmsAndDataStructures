f02ce2162ee59a3b3b816d6e23af844b2d15a7b2
wnk-123 <nickitayarmak123@gmail.com>
2024-11-04 18:24:52 +0200

    PlusOne

---
+++ src/leetcode/PlusOne.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+public class PlusOne {
+
+    private static int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] == 9) {
+                digits[i] = 0;
+            } else {
+                digits[i] = digits[i] + 1;
+                return digits;
+            }
+        }
+        int[] result = new int[digits.length + 1];
+        result[0] = 1;
+        return result;
+    }
+}
