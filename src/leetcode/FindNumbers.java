60f23de23fa9468e8ff1438fab69e29ea17a1df5
wnk-123 <nickitayarmak123@gmail.com>
2024-11-04 18:11:41 +0200

    FindNumbers

--- src/leetcode/FindNumbers.java
+++ src/leetcode/FindNumbers.java
@@ -1,4 +1,19 @@
 package leetcode;
 
 public class FindNumbers {
+
+    private static int findNumbers(int[] nums) {
+        int numberOfEven = 0;
+        for (int num: nums) {
+            int numberOfDigits = 0;
+            while (num != 0) {
+                num /= 10;
+                numberOfDigits++;
+            }
+            if (numberOfDigits % 2 == 0) {
+                numberOfEven++;
+            }
+        }
+        return numberOfEven++;
+    }
 }
