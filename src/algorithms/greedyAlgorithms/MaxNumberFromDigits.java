a795edcdec08d8448c1579a9b3af616e728e843f
wnk-123 <nickitayarmak123@gmail.com>
2024-11-05 11:35:48 +0200

    greedyAlgorithms-MaxNumberFromDigits

---
+++ src/algorithms/greedyAlgorithms/MaxNumberFromDigits.java
@@ -0,0 +1,28 @@
+package algorithms.greedyAlgorithms;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class MaxNumberFromDigits {
+
+    private static String maxNumberFromDigits(int[] digits) {
+        // {3, 1, 7, 9, 9, 5} -> {1, 3, 5, 7, 9, 9}
+        // O(n * long(n))
+
+        Arrays.sort(digits);
+        StringBuilder result = new StringBuilder();
+
+        for (int i = digits.length - 1; i >= 0; i--) {
+            result.append(digits[i]);
+        }
+        return result.toString();
+    }
+
+    private static String maxNumberFromDigitsStream(int[] digits) {
+        return String.join("", Arrays.stream(digits)
+                .boxed()
+                .sorted(Comparator.reverseOrder())
+                .map(String::valueOf)
+                .toArray(String[]::new));
+    }
+}
