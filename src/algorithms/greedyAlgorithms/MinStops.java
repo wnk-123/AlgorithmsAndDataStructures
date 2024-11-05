b154baa27d6f4a3c932dd1faebc1947bff971afb
wnk-123 <nickitayarmak123@gmail.com>
2024-11-05 14:46:40 +0200

    greedyAlgorithms-MinStops

---
+++ src/algorithms/greedyAlgorithms/MinStops.java
@@ -0,0 +1,26 @@
+package algorithms.greedyAlgorithms;
+
+public class MinStops {
+
+    private static int minStops(int[] stations, int capacity) {
+        int result = 0; // optimal number of stops
+        int currentStop = 0;
+
+        while (currentStop < stations.length - 1) {
+            int nextStop = currentStop;
+
+            while (nextStop < (stations.length -1) && stations[nextStop + 1] - stations[currentStop] <= capacity) {
+                nextStop++;
+            }
+
+            if (currentStop == nextStop) {
+                return -1;
+            }
+            if (nextStop < stations.length - 1) {
+                result++;
+            }
+            currentStop = nextStop;
+        }
+        return result;
+    }
+}
